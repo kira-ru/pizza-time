4b25cdf0ff60aa6a3dd6144e492aeb06dc8a4faa
kira-ru <kirillruban1802@mail.ru>
2023-02-09 17:53:46 +0300

    Updates

---
+++ static/js/744.fb5a0167.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza_time=self.webpackChunkpizza_time||[]).push([[744],{2744:function(n,e,r){r.r(e),r.d(e,{NotFound:function(){return c}});r(2791);var s=r(4756),i=r(184),c=function(){return(0,i.jsx)("div",{className:s.Z.wrapper,children:(0,i.jsxs)("h1",{children:[(0,i.jsx)("span",{children:"\ud83d\ude15"}),(0,i.jsx)("br",{}),"\u0422\u0430\u043a\u043e\u0439 \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u044b \u043d\u0435 \u0441\u0443\u0449\u0435\u0441\u0442\u0432\u0443\u044e\u0442"]})})}}}]);
+//# sourceMappingURL=744.fb5a0167.chunk.js.map
